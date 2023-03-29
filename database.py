bc167f2c5396c9a178c6a3c2579cea482f6467f0
Serena <serenaghini9@gmail.com>
2023-03-29 22:36:19 +0200

    Add update function

--- database.py
+++ database.py
@@ -27,27 +27,23 @@ def create_tables():
 						PRIMARY KEY (date, hour))""")
 
 
-def insert_arduino(zone, idpala, date, hour, dic):
+def insert_arduino(zone, idpala, date, hour, d):
 	connection = sqlite3.connect("database.db")
-	cursor = connection.cursor()
+	with connection:
+		cursor = connection.cursor()
 
-	stringa = f"""INSERT INTO arduino 
-				VALUES ('{zone}', '{idpala}', '{date}', '{hour}', '{dic["speed"]}', '{dic["power"]}', '{dic["current"]}', '{dic["error"]}' )"""
-	cursor.execute(stringa)
-
-	# rendo permanenti gli INSERT
-	connection.commit()
+		stringa = f"""INSERT INTO arduino 
+					VALUES ('{zone}', '{idpala}', '{date}', '{hour}', {d["speed"]}, {d["power"]}, {d["current"]}, '{d["error"]}' )"""
+		cursor.execute(stringa)
 
 
 def insert_meteo(x):
 	connection = sqlite3.connect("database.db")
-	cursor = connection.cursor()
+	with connection:
+		cursor = connection.cursor()
 
-	stringa = f"REPLACE INTO meteo VALUES ('{x[0]}', '{x[1]}', {x[2]}, '{x[3]}', '{x[4]}', '{x[5]}', '{x[6]}', '{x[7]}')"
-	cursor.execute(stringa)
-
-	# rendo permanenti gli INSERT
-	connection.commit()
+		stringa = f"REPLACE INTO meteo VALUES ('{x[0]}', '{x[1]}', {x[2]}, {x[3]}, {x[4]}, {x[5]}, {x[6]}, '{x[7]}')"
+		cursor.execute(stringa)
 
 
 def get_turbin_info(idpala):
@@ -68,9 +64,16 @@ def get_recent_meteo(n=47):
 	return result.fetchall()
 
 
+def update_power(new_val, date, hour):
+	connection = sqlite3.connect("database.db")
+	with connection:
+		cursor = connection.cursor()
+		cursor.execute(f"UPDATE meteo SET power={new_val} WHERE date='{date}' AND hour='{hour}'")
+
+
 if __name__ == '__main__':
 	# Run create_tables only if the db doesn't already exist
-	create_tables()
+	#create_tables()
 
 	dic = {
 		"current": 200,
@@ -87,6 +90,7 @@ if __name__ == '__main__':
 	m1 = ["2023-03-17", "12:00:00", 11.1, 3.05, 74, 1.0086, None, "overcast clouds"]
 	# Insert the m values inside meteo table
 	#insert_meteo(m1)
+	update_power(11.0, "2023-03-23", "12:00")
 
 	#rows = cursor.execute("SELECT name, species, tank_number FROM fish").fetchall()
 	#print(rows)
