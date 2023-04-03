b105b71811bbf58b6ff2f1b384e722c191c14bef
omaymaMoussadek <129740771+omaymaMoussadek@users.noreply.github.com>
2023-04-03 12:13:55 +0200

    Add files via upload

---
+++ WindinoWebApp/apps/__init__.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from importlib import import_module
+
+db = SQLAlchemy()
+
+def register_extensions(app):
+    db.init_app(app)
+
+def register_blueprints(app):
+    for module_name in ('home',):
+        module = import_module('apps.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+    # Impostiamo la pagina iniziale
+    @app.route('/')
+    def home():
+        return render_template('home/start.html')
+
+
+def configure_database(app):
+
+    @app.before_first_request
+    def initialize_database():
+        db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove()
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    register_extensions(app)
+    register_blueprints(app)
+    configure_database(app)
+    return app
