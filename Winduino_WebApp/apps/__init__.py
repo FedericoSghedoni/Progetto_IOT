1fa8d94f335df21a24b44ff09d5efb117f8270c5
Serena <serenaghini9@gmail.com>
2023-04-14 18:13:44 +0200

    Recover past commit

--- Winduino_WebApp/apps/__init__.py
+++ Winduino_WebApp/apps/__init__.py
@@ -31,7 +31,6 @@ def configure_database(app):
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     register_extensions(app)
     register_blueprints(app)
     configure_database(app)
